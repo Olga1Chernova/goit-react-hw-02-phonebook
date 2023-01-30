6644648bba69fb16142598ff3e596e46f95d1689
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-01-30 08:48:06 +0200

    changed file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,86 @@
 import { Component } from "react";
 import { nanoid } from "nanoid";
 
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+
 import styles from "./common.module.scss";
 
 class App extends Component {
   state = {
-    contacts: [
-      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-      {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-      {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
-    name: '',
-    number: '',
-  }
+  };
 
-  handleChange = ({target}) => {
-    const { name, value } = target;
-    this.setState({
-      [name]: value,
-    })
-  }
+  deleteContact = id => {
+    this.setState(({ contacts }) => {
+      const updatedContacts = contacts.filter(contact => contact.id !== id);
+      return { contacts: updatedContacts };
+    });
+  };
 
-  addContact = (e) => {
-    e.preventDefault();
+  addContact = ({ name, number }) => {
+    if (this.isDuplicate(name, number)) {
+      return alert(`${name} (${number}) is already in your contacts!`);
+    }
+
     this.setState(prevState => {
-      const { name, number, contacts } = prevState;
+      const { contacts } = prevState;
+
       const newContact = {
         id: nanoid(),
-        name, 
+        name,
         number,
-      }
+      };
 
-      return {contacts:[newContact, ...contacts]}
-    })
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  isDuplicate(name, number) {
+    const normalizedName = name.toLowerCase();
+    const normalizedNumber = number.toLowerCase();
+    const { contacts } = this.state;
+    const duplicate = contacts.find(({ name, number }) => {
+      return (
+        name.toLowerCase() === normalizedName ||
+        number.toLocaleLowerCase() === normalizedNumber
+      );
+    });
+    return Boolean(duplicate);
+  }
+
+  handleFilter = ({ target }) => {
+    this.setState({ filter: target.value });
+  };
+
+  getFilteredContacts() {
+    const { filter, contacts } = this.state;
+    if (!filter) {
+      return contacts;
+    }
+    const normalizedFilter = filter.toLocaleLowerCase();
+    const result = contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
+    return result;
   }
 
   render() {
-    const { addContact, handleChange } = this;
-    const { contacts } = this.state;
-    const contactInfo = contacts.map(({ id, name, number }) => {
-      return <li key={id} className={styles.contacts__item}>{name}:   {number}
-        <button className={styles.contacts__button}>Delete</button>
-      </li>
-    })
+    const { addContact, handleFilter, deleteContact } = this;
+    const contacts = this.getFilteredContacts();
+
     //console.log(contactInfo);
     return (
       <div className={styles.mainWrapper}>
         <h1 className={styles.title}>Phonebook</h1>
-        <form className={styles.form} onSubmit={addContact}>
-          <div className={styles.form__wrapper}>
-            <label className={styles.form__label}>Name</label>
-            <input
-              className={styles.form__input}
-              placeholder="Contact name"
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={handleChange}/>
-            <label className={styles.form__label}>Number</label>
-            <input
-              className={styles.form__input}
-              placeholder="Contact number"
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              onChange={handleChange}/>
-            <button className={styles.form__button} type="submit">Add contact</button>
-          </div>
-        </form>
+        <ContactForm onSubmit={addContact} />
         <h2 className={styles.title}>Contacts</h2>
-        <div className={styles.contacts__wrapper}>
-          <label className={styles.contacts__label}>Find contacts by name</label>
-          <input
-            className={styles.contacts__input}
-            placeholder="Type to find a contact"
-          ></input>
-        </div>
-        <ul className={styles.contacts}>
-          {contactInfo}
-        </ul>
+        <Filter handleChange={handleFilter} />
+        <ContactList contacts={contacts} deleteContact={deleteContact} />
       </div>
-    )
+    );
   }
 }
 export default App;
