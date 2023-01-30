6522e714b72fc929ebb7e88ca03e4929ccfae2a9
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-01-30 04:56:52 +0200

    updated phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,49 +6,84 @@ import styles from "./common.module.scss";
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
+      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
+      {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
+      {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: '',
     name: '',
-    number: ''
+    number: '',
+  }
+
+  handleChange = ({target}) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  addContact = (e) => {
+    e.preventDefault();
+    this.setState(prevState => {
+      const { name, number, contacts } = prevState;
+      const newContact = {
+        id: nanoid(),
+        name, 
+        number,
+      }
+
+      return {contacts:[newContact, ...contacts]}
+    })
   }
 
   render() {
-    
+    const { addContact, handleChange } = this;
+    const { contacts } = this.state;
+    const contactInfo = contacts.map(({ id, name, number }) => {
+      return <li key={id} className={styles.contacts__item}>{name}:   {number}
+        <button className={styles.contacts__button}>Delete</button>
+      </li>
+    })
+    //console.log(contactInfo);
     return (
       <div className={styles.mainWrapper}>
-        <h2 className={styles.title}>Phonebook</h2>
-        <form className={styles.form}>
+        <h1 className={styles.title}>Phonebook</h1>
+        <form className={styles.form} onSubmit={addContact}>
           <div className={styles.form__wrapper}>
             <label className={styles.form__label}>Name</label>
             <input
               className={styles.form__input}
+              placeholder="Contact name"
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required />
+              required
+              onChange={handleChange}/>
             <label className={styles.form__label}>Number</label>
             <input
               className={styles.form__input}
+              placeholder="Contact number"
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required />
-            <button className={styles.form__button}>Add contact</button>
+              required
+              onChange={handleChange}/>
+            <button className={styles.form__button} type="submit">Add contact</button>
           </div>
         </form>
         <h2 className={styles.title}>Contacts</h2>
-        <label className={styles.contacts__label}>Find contacts by name</label>
-        <input className={styles.contacts__input}></input>
+        <div className={styles.contacts__wrapper}>
+          <label className={styles.contacts__label}>Find contacts by name</label>
+          <input
+            className={styles.contacts__input}
+            placeholder="Type to find a contact"
+          ></input>
+        </div>
         <ul className={styles.contacts}>
-          <li className={styles.contacts__item}>Rosie Simpson: </li>
-          <li className={styles.contacts__item}>Hermione Kline:</li>
-          <li className={styles.contacts__item}>Eden Clements:</li>
+          {contactInfo}
         </ul>
       </div>
     )
